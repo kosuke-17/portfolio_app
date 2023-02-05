e5c63d35ff683ed02df942a5856661ea0d868253
kosuke-17 <tamutamu17@icloud.com>
2023-02-05 16:35:22 +0900

    remove cognit provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,4 @@
 import NextAuth, { type NextAuthOptions } from 'next-auth'
-import CognitoProvider from 'next-auth/providers/cognito'
 import GithubProvider from 'next-auth/providers/github'
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
@@ -18,16 +17,10 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    CognitoProvider({
-      clientId: process.env.COGNITO_CLIENT_ID || '',
-      clientSecret: process.env.COGNITO_CLIENT_SECRET || '',
-      issuer: process.env.COGNITO_ISSUER,
-    }),
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID || '',
       clientSecret: process.env.GITHUB_CLIENT_SECRET || '',
     }),
-    // ...add more providers here
   ],
 }
 
